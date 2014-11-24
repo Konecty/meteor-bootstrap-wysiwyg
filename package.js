2620e2ab64261e1ee5a60dfe723f6d6484eb425f
Marcelo Schmidt <marcelo@marceloschmidt.com>
2014-11-24 12:26:04 -0200

    Package

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'konecty:bootstrap-wysiwyg',
     summary: 'konecty:bootstrap-wysiwyg',
-    version: '1.0.0',
+    version: '1.1.0',
     git: 'https://github.com/Konecty/meteor-bootstrap-wysiwyg.git'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.0');
     api.addFiles([
-        'bootstrap-wysiwyg3/src/bootstrap3-wysihtml5.js',
-        'bootstrap-wysiwyg3/src/bootstrap3-wysihtml5.css',
+        'bootstrap3-wysiwyg/src/bootstrap3-wysihtml5.js',
+        'bootstrap3-wysiwyg/src/bootstrap3-wysihtml5.css',
     ], 'client');
 });
