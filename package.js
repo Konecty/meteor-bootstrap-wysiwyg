04e5408852974a6fc63546e51cf0ad577900406a
Marcelo Schmidt <marcelo@marceloschmidt.com>
2014-11-25 14:15:08 -0200

    Pointing to summernote

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
     name: 'konecty:bootstrap-wysiwyg',
-    summary: 'konecty:bootstrap-wysiwyg',
-    version: '1.1.0',
+    summary: 'WYSISWYG based off summernote',
+    version: '1.3.1',
     git: 'https://github.com/Konecty/meteor-bootstrap-wysiwyg.git'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('1.0');
     api.addFiles([
-        'bootstrap3-wysiwyg/src/bootstrap3-wysihtml5.js',
-        'bootstrap3-wysiwyg/src/bootstrap3-wysihtml5.css',
+        'summernote/dist/summernote.js',
+        'summernote/dist/summernote.css',
+        'summernote/lang/summernote-pt-BR.js',
     ], 'client');
 });
